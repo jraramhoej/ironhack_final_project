0bd2ae111d27bab9772fe4cee87fbd289093d928
Rickard Ramhoej <rickard.ramhoj@gmail.com>
2021-10-09 17:18:18 +0100

    :sparkles: Adding more pages to the web application

--- app/helper_functions.py
+++ app/helper_functions.py
@@ -7,6 +7,7 @@ import re
 import networkx as nx
 from pyvis.network import Network
 from dotenv import dotenv_values
+from statsmodels.tsa.arima.model import ARIMA
 
 # load environment variables
 config = dotenv_values(".env")
@@ -47,19 +48,29 @@ def send_response_message(user_id):
 def get_slack_data(user_id, text):
 
     # define channel id
-    try:
-        channel_id = [channel["id"] for channel in get_user_channels(user_id) if channel["name"] == text][0]
-    except:
-        channel_id = "C01T6GGTBQD"
+    # try:
+    #     channel_id = [channel["id"] for channel in get_user_channels(user_id) if channel["name"] == text][0]
+    # except:
+    #     channel_id = "C01T6GGTBQD"
 
-    # get channel history
-    result = client.conversations_history(channel=channel_id, limit=1000)
+    channel_ids = [channel["id"] for channel in get_user_channels(user_id)]
 
-    # retrieve messages
-    conversation_history = result["messages"]
+    dfs = []
 
-    # create DataFrame
-    messages = pd.DataFrame(conversation_history)
+    for channel_id in channel_ids:
+
+        # get channel history
+        result = client.conversations_history(channel=channel_id, limit=1000)
+
+        # retrieve messages
+        conversation_history = result["messages"]
+
+        # create DataFrame
+        df = pd.DataFrame(conversation_history)
+
+        dfs.append(df)
+
+    messages = pd.concat(dfs)
 
     # fill reply column with NO_REPLIES for cases where no one replied to the message
     #messages["reply_users"] = messages["reply_users"].fillna(value="NO_REPLIES")
@@ -111,9 +122,52 @@ def get_slack_data(user_id, text):
     #database.populate_table(messages)
 
 
+def time_series_analysis(df):
+
+    df['week'] = pd.to_datetime(df['ts']).dt.to_period('W')
+
+    df = df.groupby(["week"]).size().reset_index().rename(columns={0: 'count'})
+
+    df.set_index("week", inplace=True)
+
+    df.index = df.index.to_timestamp()
+
+    df.sort_index(inplace=True)
+
+    df = df.resample('W').first()
+
+    df.fillna(0, inplace=True)
+    
+    # # messages per day
+    # df = df.groupby(["ts"]).size().reset_index().rename(columns={0: 'count'})
+
+    # # convert column to datetime
+    # df['ts'] = pd.to_datetime(df['ts'])
+
+    # # make ts column into index
+    # df.index = pd.DatetimeIndex(df['ts'], freq='infer')
+
+    # # make sure we count per day and fill empty days
+    # df = df.asfreq('D')
+
+    # # remove ts column
+    # df.drop(["ts"], axis=1, inplace=True)
+
+    # # fill NaN values with 0
+    # df.fillna(0, inplace=True)
+
+    # fit model
+    model = ARIMA(df["count"], order=(5,1,0))
+    model_fit = model.fit()
+
+    #make prediction
+    predictions = model_fit.predict(start=len(df)-1, end=len(df)+3)
+
+    return {"data": df, "predictions": predictions}
+    
 
 
-def network_analysis(data):
+def network_analysis(df):
 
     # # retrieve data from database
     # results = database.sql_retrieve(database.retrieval)
@@ -123,37 +177,36 @@ def network_analysis(data):
 
     # # create pandas dataframe
     # data = pd.DataFrame(results_data, columns = [header[0] for header in description])
-
-    # save df as csv
-    data.to_csv("message_data.csv", index=False)
     
+    df = df[df["reply_users"] != "no_replies"]
+
     # get number of messages per user
-    df = data.groupby(["reply_users", "user"]).size().reset_index().rename(columns={0: 'count'})
+    df = df.groupby(["reply_users", "user"]).size().reset_index().rename(columns={0: 'count'})
 
     # get all rows of original
-    df = data.merge(df, how="left", left_on=["reply_users", "user"], right_on=["reply_users", "user"])
+    # network_df = data.merge(network_df, how="left", left_on=["reply_users", "user"], right_on=["reply_users", "user"])
 
     # rename columns
     df.rename({"reply_users": "source", "user": "target"}, inplace=True, axis=1)
 
-    df = df[["source", "target", "text", "date", "count"]]
+    # df = df[["source", "target", "text", "date", "count"]]
 
     df.reset_index(drop=True, inplace=True)
 
     # data frame for graph without messages with no replies
-    graph_df = df[df.source != "no_replies"]
+    # graph_df = df[df.source != "no_replies"]
 
     # create graph
-    Q = nx.from_pandas_edgelist(graph_df, source="source", target="target", edge_attr="count")
+    Q = nx.from_pandas_edgelist(df, source="source", target="target", edge_attr="count")
 
     # create vis network
     net = Network(height='750px', width='100%', bgcolor='white', font_color='black')
 
     net.barnes_hut()
 
-    sources = graph_df['source']
-    targets = graph_df['target']
-    weights = graph_df['count']
+    sources = df['source']
+    targets = df['target']
+    weights = df['count']
 
     edge_data = zip(sources, targets, weights)
 
@@ -166,7 +219,7 @@ def network_analysis(data):
         net.add_node(dst, dst, title=dst)
         net.add_edge(src, dst, value=w)
 
-    net.show('templates/graph.html')
+    net.show('app/templates/graph_data.html')
 
     # get the degree of centrality for the graph object
     degree_centrality = nx.degree_centrality(Q)
@@ -180,7 +233,7 @@ def network_analysis(data):
     # the density of the network
     density = nx.density(Q)
 
-    print({"degree_centrality": degree_centrality, "closeness_centrality": closeness_centrality, "betweenness_centrality": betweenness_centrality, "density": density})
+    return {"degree_centrality": degree_centrality, "closeness_centrality": closeness_centrality, "betweenness_centrality": betweenness_centrality, "density": density}
 
 
 def graph_data():
--- app/views.py
+++ app/views.py
@@ -4,56 +4,18 @@ from .models import Note, Slack
 from . import db
 import json
 import pandas as pd
-from app.helper_functions import send_response_message, get_slack_data
+from app.helper_functions import send_response_message, get_slack_data, time_series_analysis, network_analysis
 
 
 views = Blueprint('views', __name__)
 
-
+# home page with links to other pages
 @views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
+    return render_template("home.html", user=current_user)
 
-    # get the total amount of messages per team member
-    total_messages_sent = db.session.query(db.func.count(Slack.text), Slack.reply_users).group_by(Slack.reply_users).all()
-    
-    total_messages = []
-    total_members = []
-    for val, name in total_messages_sent:
-        total_messages.append(val)
-        total_members.append(name)
-
-    # get the messages sent over time
-    dates = db.session.query(db.func.count(Slack.text), Slack.ts).group_by(Slack.ts).order_by(Slack.ts).all()
-
-    over_time_messages = []
-    dates_label = []
-    for amount, date in dates:
-        dates_label.append(date.strftime("%m-%d-%y"))
-        over_time_messages.append(amount)
-
-    return render_template(
-        "home.html", 
-        user=current_user, 
-        total_messages=json.dumps(total_messages),
-        total_members=json.dumps(total_members),
-        over_time_messages=json.dumps(over_time_messages),
-        dates_label =json.dumps(dates_label)
-        )
-
-
-@views.route('/delete-note', methods=['POST'])
-def delete_note():
-    note = json.loads(request.data)
-    noteId = note['noteId']
-    note = Note.query.get(noteId)
-    if note:
-        if note.user_id == current_user.id:
-            db.session.delete(note)
-            db.session.commit()
-
-    return jsonify({})
-
+# endpoint for retrieving slack data
 @views.route("/slack", methods=["GET", "POST"])
 def slack():
 
@@ -85,8 +47,77 @@ def slack():
     
     # execute query on database from pandas
     df = pd.read_sql(query, db.session.bind)
-    print(df.head())
 
 
     return Response(), 200
 
+# page for displaying time series data
+@views.route('/message_count', methods=['GET', 'POST'])
+@login_required
+def message_count():
+
+    # load data from database
+    query = "SELECT * FROM slack;"
+    
+    # execute query on database from pandas
+    df = pd.read_sql(query, db.session.bind)
+
+    # modify data for total number of messages time series
+    time_series = time_series_analysis(df)
+    
+    # time series data
+    date_labels = list(time_series["data"].index.strftime("%m-%d-%y"))
+    over_time_messages = list(time_series["data"]["count"])
+
+    # time series prediction
+    date_labels_pred = list(time_series["predictions"].index.strftime("%m-%d-%y"))
+    over_time_messages_pred = list(time_series["predictions"])
+
+    # total messages per person
+   
+
+    #print(time_series["predictions"])
+
+
+    # get the total amount of messages per team member
+    total_messages_sent = db.session.query(db.func.count(Slack.text), Slack.reply_users).group_by(Slack.reply_users).all()
+    
+    total_messages = []
+    total_members = []
+    for val, name in total_messages_sent:
+        total_messages.append(val)
+        total_members.append(name)
+
+    # get the messages sent over time
+    # dates = db.session.query(db.func.count(Slack.text), Slack.ts).group_by(Slack.ts).order_by(Slack.ts).all()
+
+    # over_time_messages = []
+    # dates_label = []
+    # for amount, date in dates:
+    #     dates_label.append(date.strftime("%m-%d-%y"))
+    #     over_time_messages.append(amount)
+
+    return render_template(
+        "message_count.html", 
+        user=current_user, 
+        total_messages=json.dumps(total_messages),
+        total_members=json.dumps(total_members),
+        over_time_messages=json.dumps(over_time_messages),
+        date_labels =json.dumps(date_labels),
+        over_time_messages_pred=json.dumps(over_time_messages_pred),
+        date_labels_pred =json.dumps(date_labels_pred)
+        )
+
+@views.route('/graph', methods=['GET', 'POST'])
+@login_required
+def graph():
+
+    # load data from database
+    query = "SELECT * FROM slack;"
+    
+    # execute query on database from pandas
+    df = pd.read_sql(query, db.session.bind)
+
+    network_analysis(df)
+
+    return render_template("graph.html", user=current_user)
